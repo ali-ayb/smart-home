5249a313a06b062a2a47225459cfd6f2acc815b0
ali-ayb <aliayoub.me1@gmail.com>
2023-07-18 18:52:11 +0300

    added screens

---
+++ Screens/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Home = () => {
+  return <Text>Home Component</Text>;
+};
+
+export default Home;
---
+++ Screens/Setting.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Setting = () => {
+  return <Text>Setting Component</Text>;
+};
+
+export default Setting;
